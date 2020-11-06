640dc7e3b8eecd63286479b7ec01491280ec5ca9
Vladimir <vovikdoc@gmail.com>
2020-11-06 09:13:52 +0700

    fix X

--- src/CopyFiles/CustomBufferCopyFiles.java
+++ src/CopyFiles/CustomBufferCopyFiles.java
@@ -1,20 +1,49 @@
 package CopyFiles;
 
+import Progress.prgTree;
+
 import java.io.*;
 
 public class CustomBufferCopyFiles {
-    public static void copy(File input, File output)
-    {
+    public static void copy(String input, String output, int bufferLenght) throws Exception {
+        prgTree prg = new prgTree();
         try(FileInputStream fin=new FileInputStream(input);
-            FileOutputStream fos=new FileOutputStream(output))
+            FileOutputStream fos=new FileOutputStream(output, false))
         {
-            byte[] buffer = new byte[fin.available()];
-            // считываем буфер
-            fin.read(buffer, 0, buffer.length);
-            // записываем из буфера в файл
-            fos.write(buffer, 0, buffer.length);
+            int _aviable = fin.available();
+            prg = new prgTree(_aviable);
+            int current = 0;
+            int end = 0;
+            if(bufferLenght >= _aviable) {
+                bufferLenght =_aviable;
+            }
+            else{
+                end = _aviable % bufferLenght;
+            }
+            boolean flag = true;
+            while (flag) {
+                if(current > _aviable - bufferLenght)
+                {
+                    if(end == 0) break;
+                    bufferLenght = end;
+                    flag = false;
+                }
+                byte[] buffer = new byte[bufferLenght];
+                // считываем буфер
+                fin.read(buffer, 0, bufferLenght);
+                //System.out.println(pos);
+                // записываем из буфера в файл
+                fos.write(buffer, 0, bufferLenght);
+                prg.addValue(bufferLenght);
+                if(flag) current += bufferLenght;
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println(prg.getCurrentPercent() + "%\t" + prg.getStopwatches());
         }
     }
 }
--- src/Test/Main.java
+++ src/Test/Main.java
@@ -1,14 +1,19 @@
 package Test;
+import CopyFiles.CustomBufferCopyFiles;
 import Progress.prgEntity;
 import TextAnalyzation.*;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
-        analyzer anal = new analyzer("C:/Users/vovik/OneDrive/Рабочий стол/Bibliya70.txt");
+    public static void main(String[] args) throws Exception {
+       // analyzer anal = new analyzer("C:/Users/vovik/OneDrive/Рабочий стол/Bibliya70.txt");
 
-        System.out.println(anal.logger(true));
+       // System.out.println(anal.logger(true));
+        String inputFile = "C:/Users/vovik/OneDrive/Рабочий стол/Bibliya3.txt",
+                outputFile = "C:/Users/vovik/OneDrive/Рабочий стол/file_copy.txt";
+        CustomBufferCopyFiles.copy(inputFile, outputFile, 1);
     }
 }
